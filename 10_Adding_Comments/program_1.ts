11ac88219b5a2480728ce8299afdcd98a6d20159
Madiha Zeeshan <madihatajammul237@gmail.com>
2024-04-25 08:11:55 -0700

    frist commit

---
+++ 10_Adding_Comments/program_1.ts
@@ -0,0 +1,8 @@
+
+
+//Programe Written by : Madiha
+// Current Date 15-3-2024
+
+//This program will write Hello World
+
+console.log('Hello World');
---
+++ 10_Adding_Comments/program_2.ts
@@ -0,0 +1,7 @@
+
+//Program Written by: Madiha
+//Curent Date: 15-3-2024
+
+//This program will add two digits.
+
+console.log(2+2);
